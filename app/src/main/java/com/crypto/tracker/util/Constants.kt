bf0cb1be118b9a7da419d6180f7db6e7ab4fafb5
GIlyazitdinov Raphael <iraphaele@gmail.com>
2022-10-20 23:16:34 +0300

    Add "Constants" class that hold some predefined constants (Coin ids, currency).

---
+++ app/src/main/java/com/crypto/tracker/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.crypto.tracker.util
+
+/**
+ * Constants specify some test assignment requirements.
+ */
+class Constants private constructor() {
+
+    companion object {
+        const val COIN_IDS = "bitcoin,ethereum,ripple"
+        const val CURRENCIES = "usd"
+    }
+}
