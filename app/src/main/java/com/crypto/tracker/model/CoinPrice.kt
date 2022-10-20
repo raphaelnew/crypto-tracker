c3cd5c438d5e0799d9bac9354bb091894f3294fc
GIlyazitdinov Raphael <iraphaele@gmail.com>
2022-10-20 23:17:26 +0300

    Add CoinPrice/Coin data model classes.

---
+++ app/src/main/java/com/crypto/tracker/model/CoinPrice.kt
@@ -0,0 +1,51 @@
+package com.crypto.tracker.model
+
+import java.util.*
+
+/**
+ * Data model class contains information about Coin price.
+ */
+@SuppressWarnings("unused")
+data class CoinPrice(
+    val rawFields: Map<String, String>
+) {
+    val lastUpdatedAt: Long? = rawFields["last_updated_at"]?.toLongOrNull()
+
+    fun getPrice(currency: String): Double =
+        checkNotNull(getPriceOrNull(currency))
+
+    fun getPriceOrNull(currency: String): Double? =
+        rawFields[currency.lowercase()]?.toDoubleOrNull()
+
+    fun getMarketCap(currency: String): Double =
+        checkNotNull(getMarketCapOrNull(currency))
+
+    fun getMarketCapOrNull(currency: String): Double? =
+        rawFields["${currency.lowercase()}_market_cap"]?.toDoubleOrNull()
+
+    fun get24hrVol(currency: String): Double =
+        checkNotNull(get24hrVolOrNull(currency))
+
+    fun get24hrVolOrNull(currency: String): Double? =
+        rawFields["${currency.lowercase()}_24h_vol"]?.toDoubleOrNull()
+
+    fun get24hrChange(currency: String): Double =
+        checkNotNull(get24hrChangeOrNull(currency))
+
+    fun get24hrChangeOrNull(currency: String): Double? =
+        rawFields["${currency.lowercase()}_24h_change"]?.toDoubleOrNull()
+
+    fun getRawField(key: String): String? = rawFields[key]
+
+    override fun toString(): String = rawFields.toString()
+}
+
+data class CoinPrices(
+    private val rawFields: Map<String, Map<String, String>>
+) {
+
+    fun getCoins(): List<Coin> =
+        rawFields.entries.map { Coin(coinId = it.key, coinPrice = CoinPrice(it.value)) }
+}
+
+data class Coin(val coinId: String, val coinPrice: CoinPrice?)
