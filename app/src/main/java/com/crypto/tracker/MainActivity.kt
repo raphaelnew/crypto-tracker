9e057a689c954274179807fe9def6a15317f2b4e
GIlyazitdinov Raphael <iraphaele@gmail.com>
2022-10-20 23:25:15 +0300

    Update "MainActivity" to use Hilt and "MainNavHost".

--- app/src/main/java/com/crypto/tracker/MainActivity.kt
+++ app/src/main/java/com/crypto/tracker/MainActivity.kt
@@ -6,13 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.crypto.tracker.ui.navigation.MainNavHost
 import com.crypto.tracker.ui.theme.CryptoTrackerTheme
 import com.crypto.tracker.ui.theme.SystemUi
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,22 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainNavHost()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CryptoTrackerTheme {
-        Greeting("Android")
     }
 }
